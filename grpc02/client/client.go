e1d791a5745ce7e4a048011012931c46e2e950fc
liaopengjun <1337404942@qq.com>
2024-04-23 15:39:53 +0800

    stream grpc02

--- grpc02/client/client.go
+++ grpc02/client/client.go
@@ -7,6 +7,9 @@ import (
 	"io"
 	"log"
 	"micro/grpc02/service"
+	"strconv"
+	"sync"
+	"time"
 )
 
 func unAry(client service.EchoClient) {
@@ -52,6 +55,89 @@ func serveStream(client service.EchoClient) {
 	}
 }
 
+// clientStream 客户端流
+/**
+1.建立连接并获取client
+2.获取stream并通过send不断想服务端发送数据
+3.发送完成后通过stream.CloseAndRecv() 关闭steam并接收服务端返回结果
+*/
+func clientStream(client service.EchoClient) {
+	stream, err := client.ClientStreamingEcho(context.Background())
+	if err != nil {
+		log.Fatalf("client streaming echo err:%v\n", err)
+	}
+	// 发送数据
+	for i := 0; i < 100; i++ {
+		err := stream.Send(&service.EchoRequest{Message: "hello " + strconv.Itoa(i)})
+		if err != nil {
+			log.Fatalf("send echo err:%v\n", err)
+			continue
+		}
+	}
+	// 服务端返回结果
+	resp, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("CloseAndRecv err:%v\n", err)
+	}
+	log.Printf("resp:%v\n", resp.GetMessage())
+
+}
+
+// bidirectionalStream 双向流
+/*
+1. 建立连接 获取client
+2. 通过client获取stream
+3. 开两个goroutine 分别用于Recv()和Send()
+	3.1 一直Recv()到err==io.EOF(即服务端关闭stream)
+	3.2 Send()则由自己控制
+4. 发送完毕调用 stream.CloseSend()关闭stream 必须调用关闭 否则Server会一直尝试接收数据 一直报错...
+*/
+func bidirectionalStream(client service.EchoClient) {
+	var wg sync.WaitGroup
+	// 2. 调用方法获取stream
+	stream, err := client.BidirectionalStreamingEcho(context.Background())
+	if err != nil {
+		log.Fatalf("client streaming echo err:%v\n", err)
+		panic(err)
+	}
+	// 3.开两个goroutine 分别用于Recv()和Send()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			req, err := stream.Recv()
+			if err == io.EOF {
+				fmt.Println("Server Closed")
+				break
+			}
+			if err != nil {
+				continue
+			}
+			fmt.Printf("Recv Data:%v \n", req.GetMessage())
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		for i := 0; i < 2; i++ {
+			err := stream.Send(&service.EchoRequest{Message: "hello world"})
+			if err != nil {
+				log.Printf("send error:%v\n", err)
+			}
+			time.Sleep(time.Second)
+		}
+		// 4. 发送完毕关闭stream
+		err := stream.CloseSend()
+		if err != nil {
+			log.Printf("Send error:%v\n", err)
+			return
+		}
+	}()
+	wg.Wait()
+}
+
 func main() {
 	// 建立grpc链接
 	conn, err := grpc.Dial("127.0.0.1:1234", grpc.WithInsecure())
@@ -64,5 +150,7 @@ func main() {
 	client := service.NewEchoClient(conn)
 	// 调用grpc提供函数方法
 	//unAry(client)
-	serveStream(client)
+	//serveStream(client)
+	//clientStream(client)
+	bidirectionalStream(client)
 }
--- grpc02/server/server.go
+++ grpc02/server/server.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 	"micro/grpc02/service"
 	"net"
+	"sync"
 )
 
 // UnaryAPI：普通一元方法
@@ -40,6 +43,79 @@ func (e *Echo) ServerStreamingEcho(req *service.EchoRequest, stream service.Echo
 			return err
 		}
 	}
+	return nil
+}
+
+// ClientStreamingEcho 客户端流
+/*
+1. for循环中通过stream.Recv()不断接收client传来的数据
+2. err == io.EOF表示客户端已经发送完毕关闭连接了,此时在等待服务端处理完并返回消息
+3. stream.SendAndClose() 发送消息并关闭连接(虽然在客户端流里服务器这边并不需要关闭 但是方法还是叫的这个名字，内部也只会调用Send())
+*/
+func (e *Echo) ClientStreamingEcho(stream service.Echo_ClientStreamingEchoServer) error {
+	// 1.for循环接收客户端发送的消息
+	for {
+		// 2. 通过 Recv() 不断获取客户端 send()推送的消息
+		req, err := stream.Recv() // Recv内部也是调用RecvMsg
+		// 3. err == io.EOF表示已经获取全部数据
+		if err == io.EOF {
+			log.Println("client closed")
+			// 4.SendAndClose 返回并关闭连接
+			// 在客户端发送完毕后服务端即可返回响应
+			return stream.SendMsg(&service.EchoResponse{Message: "ok"})
+		}
+		if err != nil {
+			return err
+		}
+		log.Printf("Recved %v", req.GetMessage())
+	}
+}
+
+// BidirectionalStreamingEcho 双向流服务端
+/*
+// 1. 建立连接 获取client
+// 2. 通过client调用方法获取stream
+// 3. 开两个goroutine（使用 chan 传递数据） 分别用于Recv()和Send()
+// 3.1 一直Recv()到err==io.EOF(即客户端关闭stream)
+// 3.2 Send()则自己控制什么时候Close 服务端stream没有close 只要跳出循环就算close了。 具体见https://github.com/grpc/grpc-go/issues/444
+*/
+func (e *Echo) BidirectionalStreamingEcho(stream service.Echo_BidirectionalStreamingEchoServer) error {
+	var (
+		waitGroup sync.WaitGroup
+		msgCh     = make(chan string)
+	)
+	waitGroup.Add(1)
+	go func() {
+		defer waitGroup.Done()
+
+		for v := range msgCh {
+			err := stream.Send(&service.EchoResponse{Message: v})
+			if err != nil {
+				fmt.Println("Send error:", err)
+				continue
+			}
+		}
+	}()
+
+	waitGroup.Add(1)
+	go func() {
+		defer waitGroup.Done()
+		for {
+			req, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("recv error:%v", err)
+			}
+			fmt.Printf("Recved :%v \n", req.GetMessage())
+			msgCh <- req.GetMessage()
+		}
+		close(msgCh)
+	}()
+	waitGroup.Wait()
+
+	// 返回nil表示已经完成响应
 	return nil
 }
 
